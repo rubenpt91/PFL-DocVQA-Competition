05bb23ae01c00839ebadc9c190bdde2f010bf837
Ruben <rubenpt91@gmail.com>
2023-05-24 12:38:07 +0200

    Implementing Flower

--- eval_flwr.py
+++ eval_flwr.py
@@ -198,9 +198,9 @@ if __name__ == '__main__':
         strategy = fl.server.strategy.FedAvg(
             fraction_fit=1.0,  # Sample 100% of available clients for training
             fraction_evaluate=0.5,  # Sample 50% of available clients for evaluation
-            min_fit_clients=10,  # Never sample less than 10 clients for training
-            min_evaluate_clients=5,  # Never sample less than 5 clients for evaluation
-            min_available_clients=10,  # Wait until all 10 clients are available
+            min_fit_clients=NUM_CLIENTS,  # Never sample less than 10 clients for training
+            min_evaluate_clients=NUM_CLIENTS,  # Never sample less than 5 clients for evaluation
+            min_available_clients=NUM_CLIENTS,  # Wait until all 10 clients are available
             evaluate_metrics_aggregation_fn=weighted_average
         )
 
