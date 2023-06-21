6963b7cce8a3c3ba826793aa596c3d0b7f9b216a
Ruben Tito <rubenpt91@gmail.com>
2023-06-21 10:57:59 +0200

    Logger with Singleton

--- logger.py
+++ logger.py
@@ -2,7 +2,16 @@ import os, socket, datetime, getpass
 import wandb as wb
 
 
-class Logger:
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Logger(metaclass=Singleton):
 
     def __init__(self, config):
 
